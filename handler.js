9baa7174b702bf8a6eaa032cf72e62196eca5a75
Kris Quigley <kris@krisquigley.co.uk>
2018-05-18 10:55:06 +0100

    prepared code

--- handler.js
+++ handler.js
@@ -2,27 +2,62 @@
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 
 const checkForFinishedPayments = async (event, context) => {
-  const body = JSON.parse(event.body)
+  let totalPaid, totalDue, result
 
+  const body = JSON.parse(event.body)
+  
   // Set the customer id
   const customerID = {
     customer: body.data.object.customer,
     limit: 100
   }
-
+  
   try {
-    // Get the subscription plan and ID
-    const [subscriptionID, subscriptionPlan] = await getSubscriptionPlan(customerID)
-
+    const subscriptions = await stripe.subscriptions.list(customerID)
+    
+    if (subscriptions.data.length === 0) {
+      throw new Error('Customer does not have any subscriptions')
+    }
+    
+    const subscriptionID = subscriptions.data[0].id
+    const subscriptionPlan = subscriptions.data[0].items.data[0].plan.id
+    
     // Work out the total for all successful payments made
-    const totalPaid = await getTotalPaid(customerID)
-
-    // Work out the total subscription cost from the subscription
-    const totalDue = calculateTotalDue(subscriptionPlan)
-
+    const query = Object.assign({}, customerID, {paid: true, refunded: false})
+    const paidCharges = await stripe.charges.list(query)
+    
+    // Add together all payments made and return total
+    if (paidCharges.data.length > 0) {
+      totalPaid = paidCharges.data.map(item => item.amount).reduce((accumulator, item) => {
+        return accumulator + item
+      })
+    } else {
+      totalPaid = 0
+    }
+    
+    // Define the old subscription prices that no longer exist in Stripe
+    const subscriptionPrices = {
+      Instalments: 350000,
+      Parttime: 100000
+    }
+    
+    // Work out how much they need to pay in total
+    if (subscriptionPrices[subscriptionPlan]) {
+      totalDue = Number(subscriptionPrices[subscriptionPlan])
+    } else {
+      totalDue = Number(subscriptionPlan.split('_')[0])
+    }
+    
     // Cancel the subscription if they have finished paying in full
-    const result = await attemptToCancelSubscription(totalPaid, totalDue, subscriptionID)
-
+    if (totalDue === totalPaid) {
+      await stripe.subscriptions.del(subscriptionID)
+      result = 'Cancelled Subscription'
+    } else if (totalDue > totalPaid) {
+      result = `${totalDue - totalPaid} remaining`
+    } else {
+      throw new Error('Total paid is more than total due')
+    }
+    
     // Return response payload
     return {
       statusCode: 200,
@@ -32,77 +67,13 @@ const checkForFinishedPayments = async (event, context) => {
       })
     }
   } catch (error) {
-    return respondWithError(event, error)
-  }
-}
-
-const getSubscriptionPlan = async (customerID) => {
-  const subscriptions = await stripe.subscriptions.list(customerID)
-
-  if (subscriptions.data.length === 0) {
-    throw new Error('Customer does not have any subscriptions')
-  }
-
-  const subscriptionID = subscriptions.data[0].id
-  const subscriptionPlan = subscriptions.data[0].items.data[0].plan.id
-
-  return [subscriptionID, subscriptionPlan]
-}
-
-const getTotalPaid = async (customerID) => {
-  const query = Object.assign({}, customerID, {paid: true, refunded: false})
-  const paidCharges = await stripe.charges.list(query)
-  let totalPaid
-
-  // Add together all payments made and return total
-  if (paidCharges.data.length > 0) {
-    totalPaid = paidCharges.data.map(item => item.amount).reduce((accumulator, item) => {
-      return accumulator + item
-    })
-  } else {
-    totalPaid = 0
-  }
-
-  return totalPaid
-}
-
-const calculateTotalDue = (subscription) => {
-  let totalDue
-
-  // Define the old subscription prices that no longer exist in Stripe
-  const subscriptionPrices = {
-    Instalments: 350000,
-    Parttime: 100000
-  }
-
-  // Work out how much they need to pay in total
-  if (subscriptionPrices[subscription]) {
-    totalDue = subscriptionPrices[subscription]
-  } else {
-    totalDue = subscription.split('_')[0]
-  }
-
-  return Number(totalDue)
-}
-
-const attemptToCancelSubscription = async (totalPaid, totalDue, subscriptionID) => {
-  if (totalDue === totalPaid) {
-    await stripe.subscriptions.del(subscriptionID)
-    return 'Cancelled Subscription'
-  } else if (totalDue > totalPaid) {
-    return `${totalDue - totalPaid} remaining`
-  } else {
-    throw new Error('Total paid is more than total due')
-  }
-}
-
-const respondWithError = (event, error) => {
-  return {
-    statusCode: 502,
-    body: JSON.stringify({
-      message: String(error),
-      input: event
-    })
+    return {
+      statusCode: 502,
+      body: JSON.stringify({
+        message: String(error),
+        input: event
+      })
+    }
   }
 }
 
